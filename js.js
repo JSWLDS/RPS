55bddad2eb7a00bd8594285a884422dfb6262475
JSW <jswlds2@gmail.com>
2022-12-13 13:36:34 +0900

    가위바위보

--- js.js
+++ js.js
@@ -8,21 +8,22 @@ const retry = document.getElementById('retry');
 let ai_win = 0;
 let my_win = 0;
 let draW = 0;
-let cnt = 0;
+let roundCount = 1;
 
     //
 
 hand.forEach(function(element, index){
+
     element.addEventListener('click', function(){
         
         const draw_msg = document.getElementById('draw');
         const win_msg = document.getElementById('win');
         const lose_msg = document.getElementById('lose');
-        const count =  document.getElementById('count');
         const title = document.getElementById('title');
 
         var ai = Math.floor(((Math.random()*10)%3+1)) ;
         var my = index+1;
+        
         title.style.display = "none";
 
         if(my === ai){
@@ -55,7 +56,6 @@ hand.forEach(function(element, index){
             hand[2].style.display = "block";
         }
         re.style.display = "inline-block";
-        count.innerHTML = ++cnt;
         return false;
     });
 });
@@ -71,13 +71,18 @@ hand.forEach(function(element, index){
 
         const body = document.getElementById('body');
         const title = document.getElementById('title');
+        const round = document.getElementById('round');
+
+        round.innerHTML = ++roundCount;
 
         body.style.display = "flex";
         title.style.display = "block";
+
         hand.forEach((element)=>{
             element.style.display = "inline-block";
             element.style.pointerEvents = "all";
         });
+
         draw_msg.style.display = "none";
         win_msg.style.display = "none";
         lose_msg.style.display = "none";
